eb198620b1f2a5ab311fd2c2eaca86925848023c
Pahud <pahudnet@gmail.com>
2016-02-21 00:11:12 +0800

    minor improvement

--- create-name-tag.sh
+++ create-name-tag.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+echo ECS_CLUSTER=default >> /etc/ecs/ecs.config
+
+sudo yum install -y aws-cli
 az=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
 iid=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 
 region=${az::-1}
-#echo $region
-#echo $iid
 
 aws ec2 create-tags --region=$region --resources=$iid --tags Key=Name,Value="ECS - $iid"
